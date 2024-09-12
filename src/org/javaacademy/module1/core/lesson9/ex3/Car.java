297876c44f256377350a84caf028c9820bdd7b46
Sergei Rodin <sm6078@mail.ru>
2024-09-12 16:00:09 +0300

    update lessons 9 module1

---
+++ src/org/javaacademy/module1/core/lesson9/ex3/Car.java
@@ -0,0 +1,20 @@
+package org.javaacademy.module1.core.lesson9.ex3;
+
+public class Car {
+    private Engine engine = new Engine(20);
+
+    /**
+     * Вложенный статический класс (можно создать без Car)
+     */
+    public static class Engine {
+        private int power;
+
+        public Engine(int power) {
+            this.power = power;
+        }
+    }
+
+    public static void doSomething() {
+
+    }
+}
---
+++ src/org/javaacademy/module1/core/lesson9/ex5/Movable.java
@@ -0,0 +1,6 @@
+package org.javaacademy.module1.core.lesson9.ex5;
+
+public interface Movable {
+
+    void go();
+}
