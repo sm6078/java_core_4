2fcb35e4a0dad955699f487a49d14f1ae65c5b88
Sergei Rodin <sm6078@mail.ru>
2024-09-20 01:36:25 +0300

    update hw3 module 2.

---
+++ src/org/javaacademy/homeworks/module2/homework3/ex4/measured_entiry/Animal.java
@@ -0,0 +1,25 @@
+package org.javaacademy.homeworks.module2.homework3.ex4.measured_entiry;
+
+/**
+ * класс Животное
+ */
+public class Animal {
+    private final double bodyLength;
+    private final double tailLength;
+
+    public Animal(double bodyLength, double tailLength) {
+        this.bodyLength = bodyLength;
+        this.tailLength = tailLength;
+    }
+
+    public double getBodyLength() {
+        return bodyLength;
+    }
+
+    public double getTailLength() {
+        return tailLength;
+    }
+}
+
+
+
---
+++ src/org/javaacademy/homeworks/module2/homework3/ex4/measurement/Ruler.java
@@ -0,0 +1,8 @@
+package org.javaacademy.homeworks.module2.homework3.ex4.measurement;
+
+@FunctionalInterface
+public interface Ruler <V> {
+
+    Integer measure(V inputValue);
+
+}
