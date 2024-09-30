0aeaba98a8ca1a53529eed5c546edad701cee320
Sergei Rodin <sm6078@mail.ru>
2024-09-30 12:03:15 +0300

    add lesson7 module 2.

---
+++ src/org/javaacademy/module2/functional/lesson7/call_center/CallScript.java
@@ -0,0 +1,10 @@
+package org.javaacademy.module2.functional.lesson7.call_center;
+
+/**
+ * То, что нужно сказать клиенту
+ */
+@FunctionalInterface
+public interface CallScript {
+
+    void readScript();
+}
