6072a555d7b66a31f2b46a6e106d20b312c2d49a
Sergei Rodin <sm6078@mail.ru>
2024-09-30 12:02:08 +0300

    update hw4 module 2.

---
+++ src/org/javaacademy/homeworks/module2/homework4/ex1/Car.java
@@ -0,0 +1,16 @@
+package org.javaacademy.homeworks.module2.homework4.ex1;
+
+/**
+ * Класс машина
+ */
+public class Car {
+    private final String number;
+
+    public Car(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
